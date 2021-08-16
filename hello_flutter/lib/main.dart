26156ba6d175e373b1fc4aa61b3eb4ead5d239eb
Bumios <nhatduy270996@gmail.com>
2021-08-16 11:27:56 +0700

    Backup old file main for first exercise. Reset new main file for basic learning.

--- hello_flutter/lib/main.dart
+++ hello_flutter/lib/main.dart
@@ -1,114 +1,13 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      theme: ThemeData(primaryColor: Colors.lightBlueAccent),
-      home: RandomWords(),
-    );
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  const RandomWords({Key? key}) : super(key: key);
-
-  @override
-  _RandomWordsState createState() => _RandomWordsState();
-}
-
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(
-    fontSize: 18.0,
+void main() {
+  runApp(
+    const Center(
+      child: Text(
+        "Hello, world !",
+        textDirection: TextDirection.ltr,
+      ),
+    ),
   );
-  final _saved = <WordPair>{};
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: const Text('Startup name generator'), actions: [
-          IconButton(
-            icon: Icon(Icons.list),
-            onPressed: _pushSaved,
-          )
-        ]),
-        body: _buildSuggestions());
-  }
-
-  void _pushSaved() {
-    Navigator.of(context)
-        .push(MaterialPageRoute<void>(builder: (BuildContext context) {
-      final tiles = _saved.map((WordPair wordPair) {
-        return ListTile(
-            title: Text(
-          wordPair.asPascalCase,
-          style: _biggerFont,
-        ));
-      });
-      final divided = tiles.isNotEmpty
-          ? ListTile.divideTiles(
-              context: context,
-              tiles: tiles,
-            ).toList()
-          : <Widget>[];
-
-      return Scaffold(
-        appBar: AppBar(
-          title: Text('Saved Suggestions'),
-        ),
-        body: ListView(children: divided),
-      );
-    }));
-  }
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (context, i) {
-          /// `Divider widget`: 1 pixel height
-          if (i.isOdd) return Divider();
-
-          /// x ~/ y: tương đương như x / y, sẽ trả về 1 kiểu số (integer)
-          /// VD:
-          /// x là 1,2,3,4,5
-          /// y là 2
-          /// Kết quả lần lượt là 0,1,1,2,2
-          final index = i ~/ 2;
-
-          /// Nếu đã hết giá trị thì khởi tạo thêm 10 item nữa & add vào list _suggestions
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return buildRow(_suggestions[index]);
-        });
-  }
-
-  Widget buildRow(WordPair wordPair) {
-    final isSaved = _saved.contains(wordPair);
-
-    return ListTile(
-      title: Text(
-        wordPair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: Icon(
-        isSaved ? Icons.favorite : Icons.favorite_border,
-        color: isSaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (isSaved) {
-            _saved.remove(wordPair);
-          } else {
-            _saved.add(wordPair);
-          }
-        });
-      },
-    );
-  }
 }
