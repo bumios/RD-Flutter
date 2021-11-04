ad413d369f7976a2aff26eee2cae9f5cbe98ae2f
Bumios <nhatduy270996@gmail.com>
2021-11-04 17:16:52 +0700

    temporary push.

---
+++ hello_flutter/lib/Old-Resources/HelloNavigation.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(NavigationApp());
+}
+
+class NavigationApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Home screen")),
+      body: Center(
+        child: TextButton(
+          child: Text("Move to detail"),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) {
+                return DetailScreen();
+              }),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Detail screen")),
+      body: Center(
+        child: TextButton(
+          child: Text("Back to Home screen"),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
+  }
+}
